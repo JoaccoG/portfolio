63c2303974877b9a42eba2cbff65ecbc8ae4b412
JoaccoG <joaquingodoy2407@gmail.com>
2023-04-11 01:34:00 +0200

    Add example page to app router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
 
 export default router;
