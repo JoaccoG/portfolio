939313aad01c8925b431941772a9a7d250e20de9
JoaccoG <joaquingodoy2407@gmail.com>
2023-04-11 01:26:29 +0200

    Add mocked store

---
+++ src/mocks/store.tsx
@@ -0,0 +1,30 @@
+import { PreloadedState } from '@reduxjs/toolkit';
+import { render, RenderOptions } from '@testing-library/react';
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+import { AppStore, RootState, setupStore } from '../app/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    preloadedState = {
+      counter: {
+        value: 0,
+        status: 'idle',
+      },
+    },
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) => {
+  const Wrapper = ({ children }: PropsWithChildren<{}>): JSX.Element => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+};
