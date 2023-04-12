51f5b4e98e9c4531db9cd03ee33b649ca530d76e
JoaccoG <joaquingodoy2407@gmail.com>
2023-04-12 02:44:59 +0200

    Fix wip page tests

--- src/pages/WIP/WIP.test.tsx
+++ src/pages/WIP/WIP.test.tsx
@@ -1,11 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import WIP from './WIP';
 
 describe('Given a WIP page', () => {
   describe('When it is rendered', () => {
     test('Then it should match the snapshot', () => {
-      const view = render(<WIP />);
-      expect(view).toMatchSnapshot();
+      render(<WIP />);
+      expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+        /joaquín godoy/i
+      );
+      expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+        /full stack developer/i
+      );
+      expect(screen.getByText(/githubProfile/i)).toBeInTheDocument();
     });
   });
 });
