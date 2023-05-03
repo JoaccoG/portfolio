a4528c0b257fbd8e3cace72e2149491621f0df6d
JoaccoG <joaquingodoy2407@gmail.com>
2023-05-03 21:25:40 +0200

    Add code-comment shared component

---
+++ src/shared/CodeComment/CodeComment.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from 'react';
+import './CodeComment.css';
+
+interface CodeCommentProps {
+  text: string;
+}
+
+const CodeComment: React.FC<CodeCommentProps> = ({ text }) => {
+  const [lines, setLines] = useState<string[]>([]);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const hiddenRef = useRef<HTMLDivElement | null>(null);
+
+  const updateLines = () => {
+    if (!hiddenRef.current || !containerRef.current) {
+      return;
+    }
+
+    const containerWidth = containerRef.current.clientWidth;
+    const words = text.split(' ');
+    let currentLine = '';
+    let tempLines: string[] = [];
+
+    words.forEach((word) => {
+      hiddenRef.current!.innerText = currentLine + word + ' ';
+      if (hiddenRef.current!.clientWidth <= containerWidth) {
+        currentLine += word + ' ';
+      } else {
+        tempLines.push(currentLine.trim());
+        currentLine = word + ' ';
+      }
+    });
+
+    tempLines.push(currentLine.trim());
+    setLines(tempLines);
+  };
+
+  useEffect(() => {
+    const { current } = containerRef;
+    const resizeObserver = new ResizeObserver(() => {
+      updateLines();
+    });
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    updateLines();
+
+    return () => {
+      if (current) {
+        resizeObserver.unobserve(current);
+      }
+    };
+  }, [text]);
+
+  return (
+    <div className="code-comment" ref={containerRef}>
+      {lines.map((line, index) => (
+        <div key={index} className="code-comment-line">
+          <span className="code-comment-prefix">&#47;&#47; </span>
+          <span>{line}</span>
+        </div>
+      ))}
+      <div className="code-comment-hidden" ref={hiddenRef}></div>
+    </div>
+  );
+};
+
+export default CodeComment;
