5500269b4b61d1a7535e38251adb39f20bc515d8
JoaccoG <joaquingodoy2407@gmail.com>
2023-05-06 00:18:16 +0200

    Refacotr wip page structure

--- src/pages/WIP/WIP.tsx
+++ src/pages/WIP/WIP.tsx
@@ -5,37 +5,39 @@ const WIP = () => {
   return (
     <MainContainer>
       <WIPContainer>
+        <section className="wip__title">
+          <span>Hi! My name is</span>
+          <h1>Joaquín Godoy</h1>
+          <h2>&gt; Full Stack Developer</h2>
+        </section>
+        <div className="wip__link">
+          <p>
+            <span className="snippet__declaration">const</span>{' '}
+            <span className="snippet__name">githubProfile</span> ={' '}
+            <span className="snippet__code">
+              '
+              <a
+                href="https://github.com/JoaccoG"
+                target="_blank"
+                rel="noreferrer"
+                className="snippet__code--link"
+              >
+                https://github.com/JoaccoG
+              </a>
+              '
+            </span>
+          </p>
+        </div>
         <section className="wip__content">
-          <div className="wip-content__title">
-            <span>Hello world! I am</span>
-            <h1>Joaquín Godoy</h1>
-            <h2>&gt; Full Stack Developer</h2>
-          </div>
           <div className="wip-content__text">
             <CodeComment text="My portfolio is currently like a WIP painting: messy, with some bright spots and some rough edges, but slowly taking shape into a masterpiece. I'm adding brushstrokes and refining the details to make it truly reflect my style. Stay tuned for the big reveal! I promise it will be worth the wait and, who knows, maybe one day it will hang in a museum like paintings! Or, at least, on a manager's hired wall. " />
-            <p className="wip-content__text--github">
-              <span className="snippet__declaration">const</span>{' '}
-              <span className="snippet__name">githubProfile</span> ={' '}
-              <span className="snippet__code">
-                '
-                <a
-                  href="https://github.com/JoaccoG"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="snippet__code--link"
-                >
-                  https://github.com/JoaccoG
-                </a>
-                '
-              </span>
-            </p>
           </div>
-        </section>
-        <section className="wip__img">
-          <img
-            src="https://media.giphy.com/media/3iyKHMIKg5VWG6qHUm/giphy.gif"
-            alt=""
-          />
+          <div className="wip-content__img">
+            <img
+              src="https://media.giphy.com/media/3iyKHMIKg5VWG6qHUm/giphy.gif"
+              alt=""
+            />
+          </div>
         </section>
       </WIPContainer>
     </MainContainer>
