421d8ceb571bedbc8195ab7e798a708a3c874889
JoaccoG <joaquingodoy2407@gmail.com>
2023-05-08 10:16:06 +0200

    Add header component initial structure

---
+++ src/features/Header/Header.tsx
@@ -0,0 +1,74 @@
+import { NavLink } from 'react-router-dom';
+import { HeaderContainer, HeaderButton } from './Header.styled';
+import useToggle from '../../hooks/useToggle/useToggle';
+
+const Header = () => {
+  const [isOpen, toggleMenu] = useToggle(false);
+
+  return (
+    <HeaderContainer>
+      <HeaderButton
+        className={isOpen ? 'open' : ''}
+        type="button"
+        aria-label={isOpen ? 'Close menu' : 'Open menu'}
+        onClick={() => toggleMenu()}
+      >
+        <span className="hamburger-btn__line" />
+        <span className="hamburger-btn__line" />
+        <span className="hamburger-btn__line" />
+      </HeaderButton>
+      <h1 className="logo">joaquin-godoy</h1>
+      <nav>
+        <ul className="navbar">
+          <li className="navbar__item">
+            <NavLink
+              to={'./'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              data-testid="home-link"
+            >
+              _home
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to={'./about'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              data-testid="about-link"
+            >
+              _about-me
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to={'./skills'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              data-testid="skills-link"
+            >
+              _skills
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to={'./projects'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              data-testid="projects-link"
+            >
+              _projects
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to={'./contact'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              data-testid="contact-link"
+            >
+              _contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
