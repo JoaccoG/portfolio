cb58485344eeaf451b1586d30be7e0620d85671a
JoaccoG <joaquingodoy2407@gmail.com>
2023-05-08 10:17:50 +0200

    Fix use-toggle custom hook tests

--- src/hooks/useToggle/useToggle.test.tsx
+++ src/hooks/useToggle/useToggle.test.tsx
@@ -2,23 +2,25 @@ import { act, renderHook } from '@testing-library/react';
 import useToggle from './useToggle';
 
 describe('Given a useToggle custom hook', () => {
-  test('When invoked, then it returns the initial value and a function to toggle it', () => {
-    const { result } = renderHook(() => useToggle(false));
-    const [value, toggleValue] = result.current;
+  describe('When it is called', () => {
+    test('Then it should return the initial value and a function to toggle it', () => {
+      const { result } = renderHook(() => useToggle(false));
+      const [value, toggleValue] = result.current;
 
-    expect(value).toBe(false);
-    expect(typeof toggleValue).toBe('function');
-  });
-
-  test('When used, then it toggles the value correctly', () => {
-    const { result } = renderHook(() => useToggle(false));
-    const [, toggleValue] = result.current;
-
-    act(() => {
-      toggleValue();
+      expect(value).toBe(false);
+      expect(typeof toggleValue).toBe('function');
     });
 
-    const [value] = result.current;
-    expect(value).toBe(true);
+    test('Then it should toggle the value', () => {
+      const { result } = renderHook(() => useToggle(false));
+      const [, toggleValue] = result.current;
+
+      act(() => {
+        toggleValue();
+      });
+
+      const [value] = result.current;
+      expect(value).toBe(true);
+    });
   });
 });
