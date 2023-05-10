a371323f288d82155f1742a96e2b04c5721a7e94
JoaccoG <joaquingodoy2407@gmail.com>
2023-05-10 20:07:04 +0200

    Add layout page styles

---
+++ src/pages/Layout/Layout.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.main`
+  width: 100%;
+  padding: var(--padding-s);
+  margin-top: var(--margin-xxxl);
+
+  @media (min-width: 1024px) {
+    margin-top: 0;
+  }
+`;
