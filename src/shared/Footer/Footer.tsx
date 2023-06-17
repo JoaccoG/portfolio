be90aa904067346b69b4ff331f85d85474959b7b
JoaccoG <joaquingodoy2407@gmail.com>
2023-06-17 11:42:56 +0200

    change footer component structure

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,9 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { FooterContainer } from './Footer.styled';
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <h1>Footer</h1>
+      <h1 className="footer__title">find-me-in:</h1>
+      <ul className="footer__list">
+        <li className="footer-list__item">
+          <a
+            className="footer-list__item--link"
+            href="https://github.com/JoaccoG"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={brands('github')} />
+          </a>
+        </li>
+        <li className="footer-list__item">
+          <a
+            className="footer-list__item--link"
+            href="https://www.linkedin.com/in/joaquin--godoy/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={brands('linkedin-in')} />
+          </a>
+        </li>
+        <li className="footer-list__item">
+          <a
+            className="footer-list__item--link"
+            href="https://www.instagram.com/joaccog99/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={brands('instagram')} />
+          </a>
+        </li>
+      </ul>
     </FooterContainer>
   );
 };
