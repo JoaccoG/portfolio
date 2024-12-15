fb9ffd92c3bb98173f34679e14f766d0b6724cd5
JoaccoG <joaquingodoy2407@gmail.com>
2024-12-15 17:23:02 +0100

    feat(configs): add ESLint + Prettier's compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,68 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript')
+  ...compat.config({
+    extends: [
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended'
+    ],
+    ignorePatterns: [
+      'node_modules',
+      '.next',
+      'out',
+      'public',
+      'coverage',
+      'build'
+    ],
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true
+        }
+      ],
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-parens': ['error', 'always'],
+      'react/jsx-wrap-multilines': [
+        'error',
+        {
+          declaration: 'parens-new-line',
+          assignment: 'parens-new-line',
+          return: 'parens-new-line'
+        }
+      ],
+      indent: ['error', 2],
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          singleQuote: true,
+          trailingComma: 'none',
+          arrowParens: 'always',
+          bracketSpacing: true,
+          jsxBracketSameLine: false,
+          endOfLine: 'lf'
+        }
+      ],
+      'import/no-anonymous-default-export': 'off'
+    },
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'],
+        rules: {}
+      }
+    ]
+  })
 ];
 
 export default eslintConfig;
--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './'
-});
+const createJestConfig = nextJest({ dir: './' });
 
 const config: Config = {
   collectCoverage: true,
@@ -15,5 +12,4 @@ const config: Config = {
   bail: 1
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
