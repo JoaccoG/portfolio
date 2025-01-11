efa520e8be1b0eada7817e78e62beb275520bdf5
JoaccoG <joaquingodoy2407@gmail.com>
2025-01-11 20:14:23 +0100

    fix(workflows): fix github actions workflows (3rd try)

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,18 @@ const createJestConfig = nextJest({ dir: './' });
 
 /** @type {import('jest').Config} */
 const config = {
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', 'mocks/**/*.{ts,tsx}', '!src/**/*.d.ts', '!mocks/**/*.d.ts'],
+  testMatch: [
+    '<rootDir>/src/**/*.test.ts',
+    '<rootDir>/src/**/*.test.tsx',
+    '<rootDir>/mocks/**/*.test.ts',
+    '<rootDir>/mocks/**/*.test.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   bail: 1
 };
