2a55578f89966b2078ffd9d610e1e3e02c84e8ea
Joacco <joaquingodoy2407@gmail.com>
2025-02-06 12:43:26 -0300

    fix(configs): apply code rabbit suggestions

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -2,7 +2,8 @@ import { useContext } from 'react';
 import { type ThemeContextProps, ThemeContext } from '../contexts/theme/context';
 
 export const useTheme = (): ThemeContextProps => {
-  if (!ThemeContext) throw new Error('useTheme must be used within a ThemeProvider');
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error('useTheme must be used within a ThemeProvider');
 
-  return useContext(ThemeContext);
+  return context;
 };
