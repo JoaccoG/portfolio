ba1495614b622ead76e25d1966ba14ab9e9daeaa
Joacco <joaquingodoy2407@gmail.com>
2025-02-06 12:38:28 -0300

    fix(configs): fix workflows

--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 
-/**
- * Renders a component with MemoryRouter to allow testing with React Router.
- *
- * @param ui - The component to render.
- * @param initialEntries (optional) - The initial route to render.
- */
 export const renderWithMemoryRouter = (
   ui: React.ReactElement,
   { initialEntries = ['/'] }: { initialEntries?: string[] } = {}
