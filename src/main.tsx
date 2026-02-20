d6b7bd8eff7f3dee9bbf6fd6746b9ff173fccf9c
Joacco <joaquingodoy2407@gmail.com>
2026-02-20 04:46:31 -0300

    style: set variables, resets and base styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { loadUmami } from '@lib/analytics';
+import '@style/globals.css';
 
 loadUmami();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <h1 style={{ color: 'var(--color-primary)' }}>Hello World</h1>
   </StrictMode>
 );
