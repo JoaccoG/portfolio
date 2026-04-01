9b94d47f50e93c1966cd333223113f4e64623b73
Joacco <joaquingodoy2407@gmail.com>
2026-04-01 09:30:32 -0300

    feat(env): add LINKS__X environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,11 +2,12 @@
 
 interface ImportMetaEnv {
   readonly ENVIRONMENT: string;
-  readonly LINKS__EMAIL: string;
   readonly LINKS__GITHUB: string;
   readonly LINKS__LINKEDIN: string;
+  readonly LINKS__X: string;
   readonly LINKS__INSTAGRAM: string;
   readonly LINKS__SPOTIFY: string;
+  readonly LINKS__EMAIL: string;
   readonly UMAMI__SCRIPT_URL: string;
   readonly UMAMI__WEBSITE_ID: string;
 }
